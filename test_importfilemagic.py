21daa5b6c0a87e9713b56cadea42c7bab42ae5f5
Takafumi Arakaki <aka.tkf@gmail.com>
2012-08-24 22:33:08 +0200

    Add test_is_valid_module_path

--- test_importfilemagic.py
+++ test_importfilemagic.py
@@ -1,7 +1,7 @@
 import os
 import tempfile                 # to make dummy path, not to make files
 
-from nose.tools import eq_
+from nose.tools import eq_, raises
 import mock
 
 from importfilemagic import ImportFileMagic
@@ -19,3 +19,23 @@ def test_has_init():
     exists.assert_any_call(os.path.join(rootpath, 'a', 'b', '__init__.py'))
     exists.assert_any_call(os.path.join(rootpath, 'a', '__init__.py'))
     eq_(exists.call_count, 2)
+
+
+def check_is_valid_module_path(subdirs):
+    assert isinstance(subdirs, list)
+    rootpath = os.path.join(VALID_ABSPATH, 'root')
+    abspath = os.path.join(rootpath, *subdirs)
+    assert ImportFileMagic._is_valid_module_path(abspath, rootpath)
+
+
+def test_is_valid_module_path():
+    check_is_valid_module_path_fail = \
+        raises(AssertionError)(check_is_valid_module_path)
+
+    for subdirs in [['valid_module_name.py'],
+                    ['valid_directory_name', 'module.py'],
+                    ]:
+        yield (check_is_valid_module_path, subdirs)
+
+    for subdirs in [['invalid.directory.name', 'module.py']]:
+        yield (check_is_valid_module_path_fail, subdirs)
